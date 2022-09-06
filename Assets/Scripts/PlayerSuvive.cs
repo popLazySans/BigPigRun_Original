6edaaec20a5baab95743b24e25be3574d474ed5c
popLazySans <hasamu.44@gmail.com>
2022-09-06 13:49:38 +0700

    Clean_OnCollisionEnter

--- Assets/Scripts/PlayerSuvive.cs
+++ Assets/Scripts/PlayerSuvive.cs
@@ -182,9 +182,7 @@ public class PlayerSuvive : MonoBehaviour {
 			if (time > PlayerPrefs.GetFloat(rankScore4, 0)) { Update_Ranking(rankScore3, rankName3, rankScore4, rankName4, rankScore5, rankName5, scoreLock4);
 				if (time > PlayerPrefs.GetFloat(rankScore3, 0)) { Update_Ranking(rankScore2, rankName2, rankScore3, rankName3, rankScore4, rankName4, scoreLock3);
 					if (time > PlayerPrefs.GetFloat(rankScore2, 0)) { Update_Ranking(rankScore1, rankName1, rankScore2, rankName2, rankScore3, rankName3, scoreLock2);
-						if (time > PlayerPrefs.GetFloat(rankScore1, 0)) { Update_Ranking("", "", rankScore1, rankName1, rankScore2, rankName2, scoreLock1); } } } }
-		}
-
+						if (time > PlayerPrefs.GetFloat(rankScore1, 0)) { Update_Ranking("", "", rankScore1, rankName1, rankScore2, rankName2, scoreLock1); } } } }}
 	}
 	public void Update_Ranking(string rankScorePlus, string rankNamePlus, string rankScore, string rankName, string rankScoreDe, string rankNameDe, int scoreLock)
 	{
@@ -311,72 +309,12 @@ public class PlayerSuvive : MonoBehaviour {
 	
 
 	void OnCollisionEnter(Collision other){
-		if (other.gameObject.tag == "Burger") {Food_detail(other,885,2,378,204,288,0, "885 kCal", "Protein 204 kCal", "Carbohydrate 288 kCal", "Fat 378 kCal", "Vitamin 0 Energy",true,true,false,false,false,false,false,false);}
-		if (other.gameObject.tag == "Chicken") {Food_detail(other,738,1,324,360,22,0, "738 kCal", "Protein 360 kCal", "Carbohydrate 22 kCal", "Fat 324 kCal", "Vitamin 0 Energy",true,false,true,false,false,false,false,false);}
-		if (other.gameObject.tag == "Pineapple") {Food_detail(other,150,0,3,6,156,3, "150 kCal", "Protein 6 kCal", "Carbohydrate 156 kCal", "Fat 3 kCal", "Vitamin 3 Energy",true,false,false,true,false,false,false,false);}
-		if (other.gameObject.tag == "Salad") {Food_detail(other, 61, -2, 5, 12, 52, 10, "61 kCal", "Protein 12 kCal", "Carbohydrate 52 kCal", "Fat 5 kCal", "Vitamin 10 Energy", true, false, false, false, true, false, false, false);}
-		if (other.gameObject.tag == "Rice") {Food_detail(other, 390, 0, 8, 32, 336, 5, "390 kCal", "Protein 32 kCal", "Carbohydrate 336 kCal", "Fat 8 kCal", "Vitamin 0 Energy", true, false, false, false, false, true, false, false);}
-		if (other.gameObject.tag == "Watermelon") {Food_detail(other, 91, 0, 5, 7, 96, 2, "91 kCal", "Protein 7 kCal", "Carbohydrate 96 kCal", "Fat 5 kCal", "Vitamin 2 Energy", true, false, false, false, false, false, true, false);}
-		if (other.gameObject.tag == "Mushroom1") {Food_detail(other,67,-1,8,37,40,5, "67 kCal", "Protein 37 kCal", "Carbohydrate 40 kCal", "Fat 8 kCal", "Vitamin 5 Energy",true,false,false,false,false,false,false,true);}
-		if (other.gameObject.tag == "Mushroom2") {
-			Destroy (other.gameObject);
-			mushD.SetActive (true);
-			hide.SetActive (true);
-			show_died_text();
-		}
-		if (other.gameObject.tag == "Enemy") {
-			Destroy (other.gameObject);
-			point -= 1000;
-
-		}
-		if (other.gameObject.tag == "Cactus") {
-			Destroy (other.gameObject);
-			point -= 300;
-
-		}
-		if (other.gameObject.tag == "Test1") {
-			point += 1200;
-			twothor = 1;
-			Destroy (other.gameObject);
-		}
-		if (other.gameObject.tag == "Checked") {
-			show.SetActive (true);
-			AutoObjectSpawnerLock.die = 0;
-			if (point >= pointD && point <= pointP) {
-				show.SetActive (true);
-				show.GetComponent<Text> ().text = "Complete";
-				Dc = 0;
-				sc += 1;
-				trans = 0;
-				protein = 0;
-				car = 0;
-			} else {
-				show.SetActive (false);
-				docty.SetActive(true);
-				hide.SetActive(true);
-				show_died_text();
-			}
-			StartCoroutine (twoSec ());
-			Destroy (other.gameObject);
-		}
-		if (other.gameObject.tag == "Test2") {
-			show.SetActive (true);
-			AutoObjectSpawnerLock.die = 0;
-			if (point >= pointD && point <= pointP) {
-				Dc = 0;
-				sc += 1;
-				twothor = 2;
-			} else {
-				tex.GetComponent<Text> ().text = "You get score " + time.ToString ();
-				docty.SetActive (true);
-				hide.SetActive (true);
-				antiP.SetActive (false);
-				Time.timeScale = 0;
-
-			}
-			StartCoroutine (twoSec ());
-			Destroy (other.gameObject);
-		}
+		fat_group(other);
+		fruit_veget_group(other);
+		Mushroom_group(other);
+		Enemy_group(other);
+		Doctor(other);
+		tutorialObject_group(other);
 	}
 	public void Food_detail(Collision other,float point_plus,int Oil_plus,float trans_plus,float protein_plus,float car_plus,float vin_plus,string itemN_text,string itemP_text,string itemC_text,string itemO_text,string itemV_text,bool black_bool,bool I1_bool,bool I2_bool,bool I3_bool,bool I4_bool,bool I5_bool,bool I6_bool,bool I7_bool)
     {
@@ -400,6 +338,140 @@ public class PlayerSuvive : MonoBehaviour {
 		itemI5.SetActive(I5_bool);
 		itemI6.SetActive(I6_bool);
 		itemI7.SetActive(I7_bool);
+	}
+	public void fat_group(Collision other)
+    {
+		Burger(other);
+		Chicken(other);
+    }
+	public void Burger(Collision other)
+    {
+		if (other.gameObject.tag == "Burger") { Food_detail(other, 885, 2, 378, 204, 288, 0, "885 kCal", "Protein 204 kCal", "Carbohydrate 288 kCal", "Fat 378 kCal", "Vitamin 0 Energy", true, true, false, false, false, false, false, false); }
+	}
+	public void Chicken(Collision other)
+    {
+		if (other.gameObject.tag == "Chicken") { Food_detail(other, 738, 1, 324, 360, 22, 0, "738 kCal", "Protein 360 kCal", "Carbohydrate 22 kCal", "Fat 324 kCal", "Vitamin 0 Energy", true, false, true, false, false, false, false, false); }
+	}
+	public void fruit_veget_group(Collision other)
+    {
+		PineApple(other);
+		Salad(other);
+		Rice(other);
+		Watermelon(other);
+    }
+	public void PineApple(Collision other)
+    {
+		if (other.gameObject.tag == "Pineapple") { Food_detail(other, 150, 0, 3, 6, 156, 3, "150 kCal", "Protein 6 kCal", "Carbohydrate 156 kCal", "Fat 3 kCal", "Vitamin 3 Energy", true, false, false, true, false, false, false, false); }
+	}
+	public void Salad(Collision other)
+    {
+		if (other.gameObject.tag == "Salad") { Food_detail(other, 61, -2, 5, 12, 52, 10, "61 kCal", "Protein 12 kCal", "Carbohydrate 52 kCal", "Fat 5 kCal", "Vitamin 10 Energy", true, false, false, false, true, false, false, false); }
+	}
+	public void Rice( Collision other) 
+	{
+		if (other.gameObject.tag == "Rice") { Food_detail(other, 390, 0, 8, 32, 336, 5, "390 kCal", "Protein 32 kCal", "Carbohydrate 336 kCal", "Fat 8 kCal", "Vitamin 0 Energy", true, false, false, false, false, true, false, false); }
+	}
+	public void Watermelon(Collision other)
+    {
+		if (other.gameObject.tag == "Watermelon") { Food_detail(other, 91, 0, 5, 7, 96, 2, "91 kCal", "Protein 7 kCal", "Carbohydrate 96 kCal", "Fat 5 kCal", "Vitamin 2 Energy", true, false, false, false, false, false, true, false); }
+	}
+	public void Mushroom_group(Collision other)
+    {
+		White_mushroom(other);
+		Red_mushroom(other);
+    }
+	public void White_mushroom(Collision other) 
+	{
+		if (other.gameObject.tag == "Mushroom1") { Food_detail(other, 67, -1, 8, 37, 40, 5, "67 kCal", "Protein 37 kCal", "Carbohydrate 40 kCal", "Fat 8 kCal", "Vitamin 5 Energy", true, false, false, false, false, false, false, true); }
+	}
+	public void Red_mushroom(Collision other)
+	{
+		if (other.gameObject.tag == "Mushroom2")
+		{Destroy(other.gameObject);
+		mushD.SetActive(true);
+		hide.SetActive(true);
+		show_died_text();}
+	}
+	public void Enemy_group(Collision other)
+    {
+		Bird(other);
+		Cactus(other);
+    }
+	public void Bird(Collision other)
+    {
+		if (other.gameObject.tag == "Enemy")
+		{Destroy(other.gameObject);
+		point -= 1000;}
+	}
+	public void Cactus(Collision other)
+    {
+		if (other.gameObject.tag == "Cactus")
+		{Destroy(other.gameObject);
+		point -= 300;}
+	}
+	public void Doctor(Collision other)
+    {
+		if (other.gameObject.tag == "Checked")
+		{
+			show.SetActive(true);
+			AutoObjectSpawnerLock.die = 0;
+			if (point >= pointD && point <= pointP)
+			{
+				show.SetActive(true);
+				show.GetComponent<Text>().text = "Complete";
+				Dc = 0;
+				sc += 1;
+				trans = 0;
+				protein = 0;
+				car = 0;
+			}
+			else
+			{
+				show.SetActive(false);
+				docty.SetActive(true);
+				hide.SetActive(true);
+				show_died_text();
+			}
+			StartCoroutine(twoSec());
+			Destroy(other.gameObject);
+		}
+	}
+	public void tutorialObject_group(Collision other)
+    {
+		pine_test(other);
+		redMush_test(other);
+    }
+	public void pine_test(Collision other)
+    {
+		if (other.gameObject.tag == "Test1")
+		{point += 1200;
+		twothor = 1;
+		Destroy(other.gameObject);}
+	}
+	public void redMush_test(Collision other) 
+	{
+		if (other.gameObject.tag == "Test2")
+		{
+			show.SetActive(true);
+			AutoObjectSpawnerLock.die = 0;
+			if (point >= pointD && point <= pointP)
+			{
+				Dc = 0;
+				sc += 1;
+				twothor = 2;
+			}
+			else
+			{
+				tex.GetComponent<Text>().text = "You get score " + time.ToString();
+				docty.SetActive(true);
+				hide.SetActive(true);
+				antiP.SetActive(false);
+				Time.timeScale = 0;
+
+			}
+			StartCoroutine(twoSec());
+			Destroy(other.gameObject);
+		}
 	}
 	IEnumerator twoSec()
 	{
