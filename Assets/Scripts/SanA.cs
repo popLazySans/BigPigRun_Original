1a553bff312f29e702704b0948bdac274176ed00
uanetmika <untika.uanetmika@mail.kmutt.ac.th>
2022-09-20 12:11:09 +0700

    bug bar hell

--- Assets/Scripts/SanA.cs
+++ Assets/Scripts/SanA.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 public class SanA : MonoBehaviour {
 	
-	private float Transfat;
-	private float Protein;
-	private float Vitamin;
-	private float Carbohydrate;
+	public float Transfat;
+	public float Protein;
+	public float Vitamin;
+	public float Carbohydrate;
 	public Slider TransFatBar;
 	public Slider ProteinBar;
 	public Slider CarbohydrateBar;
@@ -17,32 +17,19 @@ public class SanA : MonoBehaviour {
 	public float CalculateVitamin;
 	public float CalculateCarbohydrate;
 	public KeyCode jump;
+	private calculator_nutrial caculator_n;
 	public static float lockPoint;
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		NumberTransFat();
-		NumberProtein();
-		NumberCarbohydrate();
+		
 		Nutrial();
-		NuttialBar();
 		Life();
 		Ultimate();
 	}
 
-	public void NumberTransFat() 
-	{
-		CalculateTransFat = PlayerSuvive.poL * 0.3f;
-	}
-	public void NumberProtein()
-	{
-		CalculateProtein = PlayerPrefs.GetFloat("Weight", 0) * 8;
-	}
-	public void NumberCarbohydrate()
-	{
-		CalculateCarbohydrate = (PlayerSuvive.poL - CalculateTransFat) - CalculateProtein;
-	}
+	
 	public void Nutrial()
 	{
 		Transfat = PlayerSuvive.trans;
@@ -50,16 +37,7 @@ public class SanA : MonoBehaviour {
 		Vitamin = PlayerSuvive.vin;
 		Carbohydrate = PlayerSuvive.car;
 	}
-	public void NuttialBar() 
-	{
-		TransFatBar.value = Transfat;
-		ProteinBar.value = Protein;
-		VitaminBar.value = Vitamin;
-		CarbohydrateBar.value = Carbohydrate;
-		ProteinBar.maxValue = CalculateProtein;
-		TransFatBar.maxValue = CalculateTransFat;
-		CarbohydrateBar.maxValue = CalculateCarbohydrate;
-	}
+	
 	public void Life() 
 	{
 		if (Transfat > CalculateTransFat && Protein > CalculateProtein && Carbohydrate > CalculateCarbohydrate)
