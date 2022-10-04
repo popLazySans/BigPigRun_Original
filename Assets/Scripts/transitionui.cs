b5348be0c23a5e8a27f5192b64b04d3823d6c6c2
uanetmika <untika.uanetmika@mail.kmutt.ac.th>
2022-10-04 12:01:27 +0700

    add pg dotween

---
+++ Assets/Scripts/transitionui.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class transitionui : MonoBehaviour
+{
+    float moveiubutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.DOMove(cnd)
+    }
+}
