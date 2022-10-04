4e3b7c7d678e09cd4777e4958ad93145f9dd1e04
uanetmika <untika.uanetmika@mail.kmutt.ac.th>
2022-10-04 22:36:28 +0700

    code dotween main menu

--- Assets/Scripts/transitionui.cs
+++ Assets/Scripts/transitionui.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class transitionui : MonoBehaviour
 {
-    float moveiubutton;
-    // Start is called before the first frame update
+    public Image[] alltarget;
+
     void Start()
     {
-        
+        StartCoroutine(AnimateMainUI());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator AnimateMainUI() 
     {
-        transform.DOMove(cnd)
+        for (int i = 0; i < alltarget.Length; i++) 
+        {
+            if (i != 0) 
+            {
+                yield return new WaitForSeconds(0.25f);
+            }
+            alltarget[i].GetComponentInParent<CanvasGroup>().DOFade(1, 0.25f);
+            alltarget[i].GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, 0), 0.5f);
+        }
     }
+
+
+
+
+
+
 }
