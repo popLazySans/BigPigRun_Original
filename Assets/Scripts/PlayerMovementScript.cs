36dd41a5458837efb083636a7ed6c6702a730e0b
Amiaem <112851377+Amiaem@users.noreply.github.com>
2022-10-19 16:45:31 +0700

    input system keyboard

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [System.Serializable]
 public class Done_list{
@@ -16,12 +17,15 @@ public class PlayerMovementScript : MonoBehaviour {
 	public KeyCode Jump;
     private Vector3 moveDirection;
 	public Done_list list;
+	public float moveX;
+	public float moveZ;
+	
 	void Start()
 	{
 
 	}
     void Update()
-    {
+    {	
 		point = pot;
 		lio = oil;
 		if (point <= 500) {
@@ -34,10 +38,7 @@ public class PlayerMovementScript : MonoBehaviour {
 
     void FixedUpdate()
     {
-		float moveX = Input.GetAxisRaw ("Horizontal");
-		float moveZ = Input.GetAxisRaw ("Vertical");
 		moveDirection = new Vector3(moveX, 0, moveZ).normalized;
-
 		GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * (((moveSpeed)*1000/(1+point)) - oil/50) * Time.deltaTime);
 		GetComponent<Rigidbody>().position = new Vector3
 			(
@@ -45,6 +46,7 @@ public class PlayerMovementScript : MonoBehaviour {
 				Mathf.Clamp (GetComponent<Rigidbody>().position.y, list.minY, list.maxY) 
 				,Mathf.Clamp (GetComponent<Rigidbody>().position.z, list.minZ, list.maxZ)
 			);
+
     }
 	void OnCollisionEnter(Collision other)
 	{
@@ -59,4 +61,16 @@ public class PlayerMovementScript : MonoBehaviour {
 		yield return new WaitForSeconds (3);
 		moveSpeed += 2;
 	}
+
+	public void LeftRight(InputAction.CallbackContext context) 
+	{
+		moveX = context.ReadValue<float>();
+	}
+	public void FontBack(InputAction.CallbackContext context) 
+	{
+		moveZ = context.ReadValue<float>();
+	}
+
+	
+    
 }
