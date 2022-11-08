8458132f4ca5c5adb0a1b5e99bd0bff0944436a7
ice.naruepon <ice.naruepon@gmail.com>
2022-11-08 16:15:35 +0700

    Linq : Add Sort Function And Max Energy Count

--- Assets/Scripts/Item_Detail/ShowListUI.cs
+++ Assets/Scripts/Item_Detail/ShowListUI.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShowListUI : MonoBehaviour
 {
+    [SerializeField] Text MaxEnergyText;
     [SerializeField] Transform foodListParent;
     [SerializeField] ShowFoodList foodListPrefab;
     [SerializeField] List<ShowFoodList> foodLists = new List<ShowFoodList>();
@@ -22,6 +24,14 @@ public class ShowListUI : MonoBehaviour
 	{
         DestroyAndClearAllUIs();
         CreateUIs(foodDatabase);
+
+        MaxEnergyText.text = "Max Energy : " + foodDatabase.foodDataList.Max(itemData => itemData.energy);
+    }
+
+    void AddFoodListUI(ShowFoodList foodListPrefab)
+	{
+        foodLists.Add(foodListPrefab);
+
     }
 
     void DestroyAndClearAllUIs()
@@ -33,13 +43,42 @@ public class ShowListUI : MonoBehaviour
     }
 
     void CreateUIs(FoodDatabase datas)
-    {
+    {        
         for (int i = 0; i < foodDatabase.foodDataList.Count; i++) 
 		{
             var foodUI = Instantiate(foodListPrefab, foodListParent, false);
             foodUI.gameObject.SetActive(true);
             foodUI.SetFoodData(foodDatabase.foodDataList[i]);
-
+            AddFoodListUI(foodUI);
         }
+    }
+
+    public void SortByLowestEnergy()
+    {
+        List<FoodData> sortedFoodItemDatas = foodDatabase.foodDataList.OrderBy(itemData => itemData.energy).ToList();
+        foodDatabase.foodDataList = sortedFoodItemDatas;
+		SetupUI(foodDatabase);
+
+	}
+
+	public void SortByHightestEnergy()
+    {
+        List<FoodData> sortedFoodItemDatas = foodDatabase.foodDataList.OrderByDescending(itemData => itemData.energy).ToList();
+        foodDatabase.foodDataList = sortedFoodItemDatas;
+        SetupUI(foodDatabase);
+    }
+
+    public void SortByAToZ()
+    {
+        List<FoodData> sortedFoodItemDatas = foodDatabase.foodDataList.OrderBy(itemData => itemData.name).ToList();
+        foodDatabase.foodDataList = sortedFoodItemDatas;
+        SetupUI(foodDatabase);
+    }
+
+    public void FilterVitaminOnly()
+	{
+        List<FoodData> sortedFoodItemDatas = foodDatabase.foodDataList.Where(itemData => itemData.vitamin != 0).ToList();
+        foodDatabase.foodDataList = sortedFoodItemDatas;
+        SetupUI(foodDatabase);
     }
 }
